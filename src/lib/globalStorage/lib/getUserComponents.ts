9aef2f79afc3ec357014a6187096854faed8c8f8
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-11-19 08:39:55 +0700

    Fix ensure user components file exists

--- src/lib/globalStorage/lib/getUserComponents.ts
+++ src/lib/globalStorage/lib/getUserComponents.ts
@@ -1,16 +1,8 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs-extra';
-import { USER_COMPONENTS_FILENAME } from '../config';
+import { getUserComponentsUri } from './getUserComponentsFilePath';
 
 export const getUserComponents = (context: vscode.ExtensionContext) => {
-  const userComponentsFileURI = vscode.Uri.joinPath(
-    context.globalStorageUri,
-    USER_COMPONENTS_FILENAME
-  );
-
-  if (!fs.existsSync(userComponentsFileURI.fsPath)) {
-    fs.writeJSONSync(userComponentsFileURI.fsPath, {});
-  }
-
+  const userComponentsFileURI = getUserComponentsUri(context);
   return fs.readJSONSync(userComponentsFileURI.fsPath);
 };
